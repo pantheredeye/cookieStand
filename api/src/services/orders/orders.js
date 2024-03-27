7dbd63e53c60cc8929f6909f433f7cb90388ab3d
Barrett Burnworth <77902178+pantheredeye@users.noreply.github.com>
2024-03-26 20:06:13 -0500

    Inventory Quantity Validation on Submittal; Inventory decrement on completion

--- api/src/services/orders/orders.js
+++ api/src/services/orders/orders.js
@@ -10,26 +10,61 @@ export const order = ({ id }) => {
   })
 }
 
-export const createOrder = ({ input }) => {
-  // Destructure the input to separate `orderItems` from the rest of the order data
-  const { orderItems, ...orderData } = input;
+export const createOrder = async ({ input }) => {
+  const { orderItems, ...orderData } = input
 
-  // Transform `orderItems` into the structure Prisma expects for nested creation
-  const transformedOrderItems = {
-    create: orderItems.map(item => ({
-      itemId: item.itemId,
-      quantity: item.quantity,
-    })),
-  };
+  // Start a transaction to ensure data integrity
+  return db.$transaction(async (prisma) => {
+    let insufficientItems = []
 
-  return db.order.create({
-    data: {
-      ...orderData,
-      orderItems: transformedOrderItems,
-    },
-  });
-};
+    // Check inventory levels for each item without immediately throwing an error
+    for (const orderItem of orderItems) {
+      const inventoryItem = await prisma.item.findUnique({
+        where: { id: orderItem.itemId },
+      })
 
+      if (!inventoryItem || orderItem.quantity > inventoryItem.quantity) {
+        // Collect information about insufficient inventory items
+        insufficientItems.push({
+          name: inventoryItem.name,
+          available: inventoryItem.quantity,
+        })
+      }
+    }
+
+    // If there are any insufficient items, build and throw an error message
+    if (insufficientItems.length > 0) {
+      const errorMessage =
+        insufficientItems
+          .map((item) => `${item.available} ${item.name}`)
+          .join(' and ') + ' available. Please resubmit your order.'
+      throw new Error(`Sorry, we only have ${errorMessage}`)
+    }
+
+    // Proceed with order creation and inventory update if all items are sufficient
+    const order = await prisma.order.create({
+      data: {
+        ...orderData,
+        orderItems: {
+          create: orderItems.map((item) => ({
+            itemId: item.itemId,
+            quantity: item.quantity,
+          })),
+        },
+      },
+    })
+
+    // Update inventory for each ordered item
+    for (const orderItem of orderItems) {
+      await prisma.item.update({
+        where: { id: orderItem.itemId },
+        data: { quantity: { decrement: orderItem.quantity } },
+      })
+    }
+
+    return order
+  })
+}
 
 export const updateOrder = ({ id, input }) => {
   return db.order.update({
